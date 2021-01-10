c0896e0bcd221f046049a6e7c3fd2ccbfef17d41
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2021-01-10 14:34:30 +0700

    Add contact sidebar

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,11 +1,15 @@
+import ContactBar from "../components/ContactBar";
 import Navbar from "../components/Navbar";
 
 export default function MainLayout({ children }) {
   return (
     <div>
       <Navbar />
-      <div>
-        {children}
+      <div className="flex">
+        <main className="flex-col">
+          {children}
+          <ContactBar />
+        </main>
       </div>
     </div>
   );
